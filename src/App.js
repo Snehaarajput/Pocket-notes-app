a1203a3a5729ec12d222051da7bd9561f483eeca
Rajesh Pachar <thb@Rajeshs-MacBook-Air.local>
2024-06-11 19:00:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import DesktopView from "./Views/DesktopView/DesktopView";
 import MobileView from "./Views/MobileView/MobileView";
 import NotesMobilePage from "./Components/NotesMobilePage/NotesMobilePage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Provider } from "./Context/PocketContext";
 import usePocketContext from "./Hooks/usePocketContext";
 
@@ -15,23 +15,25 @@ function App() {
     setSelected(localStorage.getItem("selected") || "");
     // eslint-disable-next-line
   }, [selected]);
-  const checkScreenSize = () => {
-    setScreenSize(window.innerWidth);
-  };
 
-  window.addEventListener("resize", checkScreenSize);
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setScreenSize(window.innerWidth);
+    };
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize); // Clean up the event listener
+  }, []);
+
   return (
     <Provider>
       <div className="App">
         {screenSize > 500 ? (
           <DesktopView />
         ) : (
-          <Router>
-            <Routes>
-              <Route path="/" element={<MobileView />} />
-              <Route path="/notes" element={<NotesMobilePage />} />
-            </Routes>
-          </Router>
+          <Routes>
+            <Route path="/" element={<MobileView />} />
+            <Route path="/notes" element={<NotesMobilePage />} />
+          </Routes>
         )}
       </div>
     </Provider>
